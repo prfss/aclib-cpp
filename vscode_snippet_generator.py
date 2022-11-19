3e0e6cae0b33b995709ebee90345b567d2c04270
prfss <gprfss4815@gmail.com>
2022-11-19 10:39:27 +0900

    Refactor vscode_snippet_generator.py

--- vscode_snippet_generator.py
+++ vscode_snippet_generator.py
@@ -3,24 +3,23 @@
 
 import sys
 import json
-import os
 from pathlib import Path
 import re
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List, Pattern
 
-CUT_BEGIN = r"^\s*//\s*<---"
-CUT_END = r"^\s*//\s*--->"
-NAME = r"^\s*//\s*name:(.+)"
+CUT_BEGIN_RE: Pattern = re.compile(r"^\s*//\s*<---")
+CUT_END_RE: Pattern = re.compile(r"^\s*//\s*--->")
+NAME_RE: Pattern = re.compile(r"^\s*//\s*name:(.+)")
 
 
-def parse(code: [str]) -> Optional[Tuple[str, str, str]]:
+def parse(block: List[str]) -> Optional[Tuple[str, str, List[str]]]:
     name = None
     prefix = None
     main_part = []
-    for line in code:
-        m = re.match(NAME, line)
-        if m:
-            name = m.group(1).strip()
+    for line in block:
+        match = NAME_RE.match(line)
+        if match:
+            name = match.group(1).strip()
             prefix = name.lower()
         else:
             main_part.append(line)
@@ -31,62 +30,58 @@ def parse(code: [str]) -> Optional[Tuple[str, str, str]]:
         return name, prefix, main_part
 
 
-def extract(source_code: str) -> [Tuple[str, str, str]]:
-    cut = False
-    codes = []
-    code = []
+def extract(source_code: str) -> List[Tuple[str, str, List[str]]]:
+    in_block = False
+    block_list = []
+    block: List[str] = []
     for s in source_code.splitlines():
-        if re.match(CUT_BEGIN, s):
-            code = []
-            cut = True
-        elif re.match(CUT_END, s):
-            if cut:
-                codes.append(code)
-                cut = False
-        elif cut:
-            code.append(s)
+        if CUT_BEGIN_RE.match(s):
+            block = []
+            in_block = True
+        elif CUT_END_RE.match(s):
+            if in_block:
+                block_list.append(block)
+                in_block = False
+        elif in_block:
+            block.append(s)
 
-    return list(filter(None, map(parse, codes)))
+    return list(filter(None, map(parse, block_list)))
 
 
-def usage():
+def usage() -> None:
     print(f"Usage: python {sys.argv[0]} <directory> <outputfile>", file=sys.stderr)
     sys.exit(1)
 
 
-def main():
+def main() -> None:
     if len(sys.argv) != 3:
         usage()
 
-    [directory, outfile] = sys.argv[1:]
+    directory = Path(sys.argv[1]).resolve()
+
+    if not directory.is_dir():
+        print(f"{directory.name} is not a directory", file=sys.stderr)
+
+    outfile = Path(sys.argv[2]).resolve()
+
     snippet = dict()
     try:
-        def absolute(file: str) -> str:
-            return os.path.join(directory, file)
-
-        files = (file for file in map(absolute, os.listdir(directory))
-                 if os.path.isfile(file))
-
-        for file in files:
-            if file.endswith(".hpp"):
+        for file in directory.iterdir():
+            if file.is_file() and file.suffix == ".hpp":
                 with open(file, "r") as f:
                     source_code = f.read()
                     extracted = extract(source_code)
                     for [name, prefix, code] in extracted:
                         print(
-                            f"Extracted {name} ({len(''.join(code))} character(s)) from {file}")
-                        stem = Path(file).stem
-                        item = dict(
-                            {
-                                "scope": "cpp",
-                                "prefix": [prefix],
-                                "body": code
-                            }
+                            f"Extracted {name} ({len(''.join(code))} character(s)) from {file.name}"
                         )
+                        stem = file.stem
+                        item = dict({"scope": "cpp", "prefix": [prefix], "body": code})
                         name = f"{name} (aclext:{stem})"
                         if name in snippet:
                             print(
-                                f"Snippet '{name}' already exists. New one is skipped")
+                                f"Snippet '{name}' already exists. New one is skipped"
+                            )
                         else:
                             snippet[name] = item
 
