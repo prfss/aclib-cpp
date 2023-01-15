ee19af07f2567428071189b9eeaa085a1f131a3f
prfss <gprfss4815@gmail.com>
2023-01-15 14:18:50 +0900

    Add CumulativeSum2D test

---
+++ test/cumulative_sum_2d_test.cpp
@@ -0,0 +1,14 @@
+#include "aclext/cumulative_sum_2d.hpp"
+#include "gtest/gtest.h"
+#include <vector>
+
+using namespace aclext;
+using namespace std;
+
+TEST(CumulativeSum2DTest, SmallExample) {
+    vector<vector<int>> l = { { 1, 2, 3 }, { 4, 5, 6 }, { 8, 9, 10 } };
+    CumulativeSum2D<int> cs(l);
+
+    EXPECT_EQ(cs(0, 0, 2, 2), 48);
+    EXPECT_EQ(cs(1, 1, 2, 2), 30);
+}
