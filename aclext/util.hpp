e17a85fef5bd7b721f51d476139ccaab1dd85097
prfss <gprfss4815@gmail.com>
2023-01-15 14:11:44 +0900

    Add const modifier

--- aclext/util.hpp
+++ aclext/util.hpp
@@ -4,7 +4,7 @@ namespace aclext {
 // <---
 // name: next LE
 template <typename T, typename S>
-T next_le(S& s, T v, T def) {
+T next_le(const S& s, T v, T def) {
     auto it = s.upper_bound(v);
     if (it == s.begin()) {
         return def;
@@ -18,7 +18,7 @@ T next_le(S& s, T v, T def) {
 // <---
 // name: next GE
 template <typename T, typename S>
-T next_ge(S& s, T v, T def) {
+T next_ge(const S& s, T v, T def) {
     auto it = s.lower_bound(v);
     if (it == s.end()) {
         return def;
