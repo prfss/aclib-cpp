bfe8727c600000e380f7f0a59242f14e1bb6a83b
prfss <gprfss4815@gmail.com>
2023-06-17 09:35:15 +0900

    Update sieve.hpp

--- aclext/sieve.hpp
+++ aclext/sieve.hpp
@@ -14,7 +14,7 @@ struct Sieve {
         is_prime.resize(n + 1);
         fill(is_prime.begin(), is_prime.end(), true);
         is_prime[0] = is_prime[1] = false;
-        for (int i = 2; i <= n; i++) {
+        for (long long i = 2; i <= n; i++) {
             if (is_prime[i]) {
                 primes.push_back(i);
                 for (long long j = i * i; j <= n; j += i) {
