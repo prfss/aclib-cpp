a9a8f04520e8034d3a687b7922e84e7cc9e9a5bc
prfss <gprfss4815@gmail.com>
2024-10-28 23:51:41 +0900

    random.hppの更新

--- aclib/random.hpp
+++ aclib/random.hpp
@@ -8,48 +8,9 @@
 #include <cfloat>
 #include <cmath>
 #include <random>
-#include <type_traits>
+#include <vector>
 
 using namespace std;
-
-// <---
-// name: rand_range
-/// 整数区間@f$[l,r)@f$から一様に数を選んで返します.
-/// ### 制約
-/// - @f$l \lt r@f$
-template <typename T, typename R>
-inline enable_if_t<is_integral_v<T>, T> rand_range(T l, T r, R& rng) {
-    assert(l < r);
-    return uniform_int_distribution<T>(l, r - 1)(rng);
-}
-
-/// 実数区間@f$[l,r)@f$から一様に数を選んで返します.
-/// ### 制約
-/// - @f$l \lt r@f$
-template <typename T, typename R>
-inline enable_if_t<is_floating_point_v<T>, T> rand_range(T l, T r, R& rng) {
-    assert(l < r);
-    return uniform_real_distribution<double>(l, r)(rng);
-}
-
-template <typename T>
-inline T right_closed(T r) {
-    return nextafter(r, numeric_limits<T>::max());
-}
-// --->
-
-// <---
-// name: rand_bool
-/// @f$p@f$の確率で`true`を,@f$1 - p@f$の確率で`false`を返します.
-/// ### 制約
-/// - @f$0 \le p \le 1@f$
-template <typename R>
-inline bool rand_bool(double p, R& rng) {
-    assert(0.0 <= p and p <= 1.0);
-    static auto dist = uniform_real_distribution<double>(0.0, 1.0);
-    return dist(rng) < p;
-}
-// --->
 
 // <---
 // name: Weighted Index
@@ -58,31 +19,35 @@ template <typename T>
 class WeightedIndex {
 private:
     const size_t n;
-    vector<T> cumulative_weight;
+    vector<double> cumulative_weight;
 
 public:
     /// 重みを`weights`として初期化します.
     /// ### 制約
-    /// - @f$0 \le \mathrm{weights[i]} (0 \le i \lt \mathrm{weights.size()})@f$
-    /// - @f$0 \lt \sum_{i} \mathrm{weights[i]} \le Tの最大値@f$
+    /// - @f$0 \le \mathrm{weights[i]} \ (0 \le i \lt \mathrm{weights.size()})@f$
+    /// - @f$0 \lt \sum_{i} \mathrm{weights[i]} \lt \mathrm{DBL\_MAX}@f$
     WeightedIndex(const vector<T>& weights) :
         n(weights.size()) {
         assert(weights.size() > 0);
         cumulative_weight.resize(n);
-        partial_sum(weights.begin(), weights.end(), cumulative_weight.begin());
+        partial_sum(weights.begin(), weights.end(), cumulative_weight.begin(), [](double a, T b) { return a + b; });
         assert(cumulative_weight.back() > 0);
+    }
+
+    size_t size() const {
+        return n;
     }
 
     /// インデックスを重みに比例する確率で選択して返します.
     /// `rng`は[乱数エンジン](https://ja.cppreference.com/w/cpp/numeric/random#.E4.B9.B1.E6.95.B0.E3.82.A8.E3.83.B3.E3.82.B8.E3.83.B3)です.
     template <typename R>
     size_t operator()(R& rng) const {
-        T x = rand_range<T>(T(0), cumulative_weight.back(), rng);
-
+        auto dist = uniform_real_distribution<double>(0.0, cumulative_weight.back());
+        double x = dist(rng);
         long long l = -1, r = n - 1;
         while (r - l > 1) {
             auto m = (l + r) / 2;
-            T v = cumulative_weight[m];
+            double v = cumulative_weight[m];
             if (x >= v)
                 l = m;
             else
--- test/random_test.cpp
+++ test/random_test.cpp
@@ -2,16 +2,54 @@
 #include "gtest/gtest.h"
 #include <random>
 
-TEST(RandomTest, Simple) {
-    mt19937 rng(12345);
+const int SEED = 31415926;
 
-    for (int i = 0; i < 10000; i++) {
-        EXPECT_EQ(1, rand_range(1, 2, rng));
-        EXPECT_DOUBLE_EQ(1.0, rand_range(1.0, nextafter(1.0, DBL_MAX), rng));
+TEST(RandomTest, WeightedIndexDouble) {
+    mt19937_64 rng(SEED);
+    vector<double> weights = { 1.0, 1.5, 2.0, 10.0, 1.01, 7.5, 9.5 };
+    auto dist = WeightedIndex(weights);
+    const int n = 1000000;
+    vector<int> c(dist.size());
+    for (int i = 0; i < n; i++) {
+        size_t k = dist(rng);
+        EXPECT_TRUE(0 <= k && k < dist.size());
+        c[k]++;
     }
 
-    for (int i = 0; i < 10000; i++) {
-        EXPECT_TRUE(rand_bool(1.0, rng));
-        EXPECT_FALSE(rand_bool(0.0, rng));
+    // 確率的
+    double ws = accumulate(weights.begin(), weights.end(), 0.0);
+    for (size_t k = 0; k < dist.size(); k++) {
+        double r = 1.0 * c[k] / n;
+        // fprintf(stderr, "%f =~ %f\n", r, weights[k] / ws);
+        long nearest = min_element(weights.begin(), weights.end(), [&](double a, double b) {
+                           return abs(a / ws - r) < abs(b / ws - r);
+                       })
+                       - weights.begin();
+        EXPECT_EQ(nearest, k);
+    }
+}
+
+TEST(RandomTest, WeightedIndexUInt) {
+    mt19937_64 rng(SEED);
+    vector<uint> weights = { 1, 2, 5, 3, 10, 8, 100 };
+    auto dist = WeightedIndex(weights);
+    const int n = 1000000;
+    vector<int> c(dist.size());
+    for (int i = 0; i < n; i++) {
+        size_t k = dist(rng);
+        EXPECT_TRUE(0 <= k && k < dist.size());
+        c[k]++;
+    }
+
+    // 確率的
+    double ws = accumulate(weights.begin(), weights.end(), 0.0);
+    for (size_t k = 0; k < dist.size(); k++) {
+        double r = 1.0 * c[k] / n;
+        // fprintf(stderr, "%f =~ %f\n", r, weights[k] / ws);
+        long nearest = min_element(weights.begin(), weights.end(), [&](double a, double b) {
+                           return abs(a / ws - r) < abs(b / ws - r);
+                       })
+                       - weights.begin();
+        EXPECT_EQ(nearest, k);
     }
 }
